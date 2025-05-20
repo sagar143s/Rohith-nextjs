f3175119ae5719421a76b5f029ebfb118b6a869c
ROHITH SAGAR M <rohithsagar14325@gmail.com>
2025-05-20 13:33:14 +0400

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -65,7 +65,7 @@ export default function Intro() {
         animate={{ opacity: 1, y: 0 }}
       >
         <span className="font-bold">Hello, I'm Rohith Sagar M.</span> I'm a{" "}
-        <span className="font-bold">Front-End developer</span> with{" "}
+        <span className="font-bold">Full-Stack Developer</span> with{" "}
         <span className="font-bold">5 years</span> of experience. I enjoy
         building <span className="italic">Websites & Android & IOS apps</span>. 
       </motion.h1>
